917a357c230b83aecc3a9424be54fb30719da667
Cabins <cabins.code@gmail.com>
2024-12-10 16:03:22 +0800

    Update bashrc-appendix.sh

--- bashrc-appendix.sh
+++ bashrc-appendix.sh
@@ -1,23 +1,23 @@
 
 function update-os {
-    echo -e ">>> Upgrading OS..."
+    echo -e "\n⁂ Upgrading OS..."
     pkg update
     pkg upgrade -y
 
-    echo -e ">>> Cleaning packages..."
+    echo -e "\n⁂ Cleaning packages..."
     pkg autoclean
     pkg clean
 
-    echo -e ">>> Done."
+    echo -e "\n⁂ Done."
 }
 
 function update-pip {
     PIP_OUTDATED_PACKAGES=$(pip list --outdated | tail -n +3)
     if [ "" == "$PIP_OUTDATED_PACKAGES" ]; then
-        echo "Python packages are already latest."
+        echo -e "\n⁂ Python packages are already latest."
         return
     fi
 
     echo $PIP_OUTDATED_PACKAGES | awk '{print $1}' | xargs pip install -U
-    echo "Pip upgrade done."
+    echo -e "\n⁂ Pip upgrade done."
 }
