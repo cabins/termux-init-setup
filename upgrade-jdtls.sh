43a674f9a7c571b259b57ef395cf72b3cb0ecac4
Cabins <cabins.code@gmail.com>
2024-12-12 08:26:25 +0800

    Update upgrade-jdtls.sh

--- upgrade-jdtls.sh
+++ upgrade-jdtls.sh
@@ -1,4 +1,9 @@
 
+log-echo() {
+    echo -e "♪♪♪ $1\n"
+}
+
+
 JDTLS_SERVER='https://download.eclipse.org/jdtls/snapshots'
 LATEST_FILE_TXT='/latest.txt'
 LATEST_FILE_LOCAL='jdt-language-server-latest.tar.gz'
@@ -10,25 +15,25 @@ upgrade_jdtls() {
     if [ -f $LATEST_FILE_LOCAL ]; then
         LOCAL_FILE_SHA256=$(sha256sum $LATEST_FILE_LOCAL)
         if [ "$LATEST_FILE_SHA256" == "$LATEST_FILE_SHA256" ]; then
-            echo "Latest file already exists."
+            log-echo "Latest file already exists."
             return
         fi
 
     else
-        echo "Latest file does NOT exist, now downloading..."
+        log-echo "Latest file does NOT exist, now downloading..."
     fi
 
     wget -c ${JDTLS_SERVER}/${LATEST_FILE_NAME} -O tmp.tar.gz
     mv tmp.tar.gz $LATEST_FILE_LOCAL
 
-    echo "Unzipping file..."
+    log-echo "Unzipping file..."
     if [ -d "jdtls" ]; then
         rm -rf jdtls
     fi
 
     mkdir jdtls
     tar -xvf $LATEST_FILE_LOCAL -C jdtls
-    echo "Done."
+    log-echo "Done."
 }
 
 upgrade_jdtls
