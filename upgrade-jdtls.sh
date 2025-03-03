75ea90e2f612649c9f73e15b45bffd6ca33da4d8
Cabins <cabins.code@gmail.com>
2025-03-03 16:43:12 +0800

    Update upgrade-jdtls.sh

--- upgrade-jdtls.sh
+++ upgrade-jdtls.sh
@@ -1,4 +1,3 @@
-
 log-echo() {
     echo -e "\n🍒🍒🍒 $1\n"
 }
@@ -6,14 +5,17 @@ log-echo() {
 
 JDTLS_SERVER='https://download.eclipse.org/jdtls/snapshots'
 LATEST_FILE_TXT='/latest.txt'
-LATEST_FILE_LOCAL='jdt-language-server-latest.tar.gz'
 
 upgrade_jdtls() {
     # if [ -f $LATEST_FILE_LOCAL ];
     LATEST_FILE_NAME=$(curl -s ${JDTLS_SERVER}${LATEST_FILE_TXT})
     LATEST_FILE_SHA256=$(curl -s ${JDTLS_SERVER}/${LATEST_FILE_NAME}.sha256)
-    if [ -f $LATEST_FILE_LOCAL ]; then
-        LOCAL_FILE_SHA256=$(sha256sum $LATEST_FILE_LOCAL)
+    echo -e "LatestFile $LATEST_FILE_NAME\n  " | column -t
+    if [ -f $LATEST_FILE_NAME ]; then
+        LOCAL_FILE_SHA256=$(sha256sum $LATEST_FILE_NAME | awk '{print $1}')
+        echo -e "ExpectSHA256 $LATEST_FILE_SHA256\n
+                LocalSHA256 $LOCAL_FILE_SHA256" | column -t
+
         if [ "$LATEST_FILE_SHA256" == "$LATEST_FILE_SHA256" ]; then
             log-echo "Latest file already exists."
             return
@@ -23,8 +25,7 @@ upgrade_jdtls() {
         log-echo "Latest file does NOT exist, now downloading..."
     fi
 
-    wget -c ${JDTLS_SERVER}/${LATEST_FILE_NAME} -O tmp.tar.gz
-    mv tmp.tar.gz $LATEST_FILE_LOCAL
+    wget -q --show-progress -c ${JDTLS_SERVER}/${LATEST_FILE_NAME} -O $LATEST_FILE_NAME
 
     log-echo "Unzipping file..."
     if [ -d "jdtls" ]; then
@@ -32,7 +33,7 @@ upgrade_jdtls() {
     fi
 
     mkdir jdtls
-    tar -xvf $LATEST_FILE_LOCAL -C jdtls
+    tar -xvf $LATEST_FILE_NAME -C jdtls
     log-echo "Done."
 }
 
